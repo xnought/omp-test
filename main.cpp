e162e0c89cf27dc5dc9cb5c8ae03ac5f6c07b2a7
xnought <drbertucci@hotmail.com>
2025-03-24 20:03:20 -0400

    multiple threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main()
 {
-	printf("Hello!");
+	int total_threads = omp_get_num_procs();
+	printf("Total number of threads: %d\n", total_threads);
+
+#pragma omp parallel
+	{
+		int id = omp_get_thread_num();
+		printf("Hello from %d\n", id);
+	}
 }
